62ee15c7d9747cf13ca20bd78ca2db5dd252eab7
ufukAkyuzYildirim <yildirimufuk.esogu@gmail.com>
2022-01-27 20:31:27 +0300

    folder organization.

--- FullCourse/main.go
+++ FullCourse/main.go
@@ -1,7 +1,16 @@
 package main
 
-import httppackage "golangTutorial/httpPackage"
-
 func main() {
-	httppackage.HttpMain()
+
 }
+
+var portNumber = ":8080"
+
+// //main http package example function.
+// func HttpMain() {
+// 	http.HandleFunc("/", handlers.Home)
+// 	http.HandleFunc("/about", handlers.About)
+// 	fmt.Println(fmt.Sprintf("starting application on port: %s", portNumber))
+// 	// fmt.Printf(fmt.Sprintf("starting application on port: %s\n", portNumber))
+// 	_ = http.ListenAndServe(portNumber, nil)
+// }
