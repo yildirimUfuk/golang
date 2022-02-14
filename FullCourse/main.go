aacb40b8c1f501fd5020861540570e333193c0e7
yildirimUfuk <yildirimUfuk.esogu@gmail.com>
2022-02-14 21:07:28 +0300

    added some scruct example.

--- FullCourse/main.go
+++ FullCourse/main.go
@@ -1,28 +1,13 @@
 package main
 
-import "fmt"
+type indicies struct {
+	x int
+	y int
+	z int
+}
 
 func main() {
-	// web.HttpMain()
-	digits := []int{1, 2, 1, 4, 3}
-
-	fmt.Println(plusOne(digits))
-}
-func plusOne(digits []int) []int {
-	sol := []int{0}
-	sol = append(sol, digits...)
-	for i := len(sol) - 1; i >= 0; i-- {
-		sol[i]++
-		if sol[i] > 9 {
-			sol[i] = 0
-			continue
-		}
-		break
-	}
-	if sol[0] == 0 {
-		sol = sol[1:len(sol)]
-	}
-	return sol
+	ch := make(chan int)
 }
 
 // var portNumber = ":8080"
